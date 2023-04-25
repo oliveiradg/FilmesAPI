41a26417d39d9b451f905cc4dfb514bcf2524bcd
João Oliveira <j.oliveiradg@gmail.com>
2023-04-24 22:11:32 -0300

    Project Create

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+public class FilmeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
