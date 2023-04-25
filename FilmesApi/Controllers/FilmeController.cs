c1ebb785090f844e6c750f87161f0ef45457365a
João Oliveira <j.oliveiradg@gmail.com>
2023-04-24 23:08:58 -0300

    [DIA 106] feat: Recebendo os dados de um filme

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -1,11 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FilmesApi.Controllers;
-
-public class FilmeController : Controller
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
 {
-    public IActionResult Index()
+    private static List<Filme> filmes = new List<Filme>();
+
+    [HttpPost]
+    public void AdicionaFilme([FromBody]Filme filme)
     {
-        return View();
+        filmes.Add(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Duracao);
     }
 }
---
+++ FilmesApi/Models/Filme.cs
@@ -0,0 +1,9 @@
+﻿namespace FilmesApi.Models;
+
+public class Filme
+{
+    public string Titulo { get; set; }
+    public string Genero { get; set; }
+
+    public int Duracao { get; set; }
+}
